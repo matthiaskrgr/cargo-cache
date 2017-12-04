ffc692f567d4b318c2b3bbf9a700f41b9ba175d9
Matthias Krüger <matthias.krueger@famsik.de>
2017-12-04 20:22:49 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
 extern crate walkdir;
 
-//use std::env;
-//use std::io;
-use walkdir::WalkDir;
-//use std::path::Path;
-//use std::fs::{self, DirEntry};
 use std::fs;
+use walkdir::WalkDir;
 
 
-
-// let metadata = fs::metadata("foo.txt")?;
-// let size =  metadata.len()
 fn main() {
 
-
-//    let mut paths_to_check = Vec::new();
-    //let mut files_to_check = Vec::new();
     let cargo_dir = "/home/matthias/.cargo/";
 
     let mut cumulative_size = 0;
