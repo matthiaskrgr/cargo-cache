932ac1cf30086f70c9762629ff246413fb8206aa
Matthias Krüger <matthias.krueger@famsik.de>
2017-12-04 20:35:40 +0100

    abort if we can't find cargo dir

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ extern crate humansize;
 extern crate walkdir;
 
 use std::fs;
+use std::path::Path;
 use humansize::{FileSize, file_size_opts as options};
 use walkdir::WalkDir;
 
 fn main() {
-
     let cargo_dir = "/home/matthias/.cargo/";
+
+    if !Path::new(cargo_dir).is_dir() {
+        println!("Error, no '~/.cargo/' dir found");
+        std::process::exit(1);
+    }
 
     let mut cumulative_size = 0;
 
@@ -21,12 +26,15 @@ fn main() {
         }
 
     }
+
+
     println!("Cargo cache:\n\n");
     println!("Total size: {} b", cumulative_size);
     println!(
         "Total size: {} ",
         cumulative_size.file_size(options::DECIMAL).unwrap()
     );
+
 
 
 }
