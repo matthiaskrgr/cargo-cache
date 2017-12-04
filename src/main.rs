fd8f71c1b126632f34051fea323e043244728e2d
Matthias Krüger <matthias.krueger@famsik.de>
2017-12-04 20:19:04 +0100

    getting cumulative file size works now.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-//extern crate walkdir;
+extern crate walkdir;
 
 //use std::env;
 //use std::io;
-//use walkdir::WalkDir;
+use walkdir::WalkDir;
 //use std::path::Path;
 //use std::fs::{self, DirEntry};
 use std::fs;
@@ -12,31 +12,27 @@ use std::fs;
 // let metadata = fs::metadata("foo.txt")?;
 // let size =  metadata.len()
 fn main() {
-    let mut paths_to_check = Vec::new();
-    let mut files_to_check = Vec::new();
 
+
+//    let mut paths_to_check = Vec::new();
+    //let mut files_to_check = Vec::new();
     let cargo_dir = "/home/matthias/.cargo/";
-    //let cargo_dir = ".";
 
-    for entry in fs::read_dir(cargo_dir).unwrap() {
-        //println!("{:?}", entry);
+    let mut cumulative_size = 0;
+
+    for entry in WalkDir::new(cargo_dir) {
         let entry = entry.unwrap();
         let path = entry.path();
-        if path.is_dir() {
-            paths_to_check.push(path);
-        } else if path.is_file() {
-            files_to_check.push(path);
+        //println!("{}", path.display());
+
+        if path.is_file() {
+            cumulative_size += fs::metadata(path).unwrap().len();
         }
 
-    } // for
+        //files_to_check.push(path.clone());
 
-    println!{"paths to check {:?}", paths_to_check};
-    println!{"files to check {:?}", files_to_check};
+    }
+    println!("Total size: {} b", cumulative_size );
 
 
 }
-
-
-
-
-// one possible implementation of walking a directory only visiting files
