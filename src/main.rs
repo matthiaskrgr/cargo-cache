f8c35f2d3b7da8208ce35724635f922920f56d47
Matthias Krüger <matthias.krueger@famsik.de>
2017-12-06 17:04:04 +0100

    WIP: deletion of directories

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,28 @@ extern crate cargo; // obtain CARGO_DIR
 
 use std::fs;
 use std::path::Path;
+use std::io;
 
 use clap::{Arg, App};
 use humansize::{FileSize, file_size_opts as options};
 use walkdir::WalkDir;
 //use cargo::util::config;
+
+
+struct CacheDir<'a> {
+    path: &'a std::path::Path, // path object of the dir
+    string: &'a str, // string that represents the dir path
+}
+
+struct CacheDirCollector<'a> {
+    // an object containing all the relevant cache dirs
+    // for easy pasing around to functions
+    GitCheckouts: &'a CacheDir<'a>,
+    GitDB: &'a CacheDir<'a>,
+    Registry: &'a CacheDir<'a>,
+    BinDir: &'a CacheDir<'a>,
+}
+
 
 fn cumulative_dir_size(dir: &str) -> u64 {
     //@TODO add some clever caching
@@ -43,25 +60,99 @@ fn get_file_number(dir: &str) -> u64 {
     number_of_files
 }
 
+
+
+
+fn rm_dir(cache: &CacheDirCollector) {
+    // remove a directory from cargo cache
+    let mut dir_to_delete: &CacheDir;
+
+    println!("Possile directories to delete: 'git-checkouts', 'git-db', 'registry', 'all'.");
+    println!("'abort' to abort.");
+
+    'inputStrLoop: loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).ok().expect(
+            "Couldn't read input",
+        );
+
+        // check what dir we are supposed to delete now
+        match input.trim() {
+            "git-checkouts" => {
+                dir_to_delete = cache.GitCheckouts;
+                break;
+            }
+            "git-db" => {
+                dir_to_delete = cache.GitDB;
+                break;
+            }
+            "registry" => {
+                dir_to_delete = cache.Registry;
+                break;
+            }
+            "bin-dir" => println!("Please use 'cargo uninstall'."),
+            _ => {
+                println!("Invalid input.");
+                println!("Possile directories to delete: 'git-checkouts', 'git-db', 'registry'.");
+                continue 'inputStrLoop;
+            } // _
+        } // match input
+    } // 'inputStrLoop
+
+
+    println!("Trying to delete {}", dir_to_delete.string);
+    println!("Really delete dir {} ? (yes/no)", dir_to_delete.string);
+
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).ok().expect(
+            "Couldn't read line",
+        );
+        if input.trim() == "yes" {
+            println!("deleting {}", dir_to_delete.string);
+
+            // rm
+            // @TODO https://doc.rust-lang.org/std/fs/fn.remove_dir_all.html
+
+            break;
+        } else if input == "no" {
+            println!("keeping {}", dir_to_delete.string);
+            break;
+        } else {
+            println!("Invalid input: {}", input);
+            println!("Please use 'yes' or 'no'");
+        }
+    } // loop
+
+
+} // fn rm_dir
+
+
+
+
 fn main() {
 
     let cargo_show_cfg = App::new("cargo-show")
         .version("0.1")
         .about("Manage cargo cache")
         .author("matthiaskrgr")
+        .arg(Arg::with_name("print-dirs").short("d").long("dirs").help(
+            "Show found directory paths.",
+        ))
         .arg(
-            Arg::with_name("print-dirs")
-                .short("d")
-                .long("dirs")
-                .help("Show found directory paths."),
+            Arg::with_name("remove-dirs")
+                .short("r")
+                .long("remove")
+                .help("Remove directories in the cache."),
         )
         .get_matches();
-
 
 
     let cargo_cfg = cargo::util::config::Config::default().unwrap();
     let cargo_home_str = format!("{}", cargo_cfg.home().display());
     let cargo_home_path = Path::new(&cargo_home_str);
+
+
 
 
     // make sure we actually have a cargo dir
@@ -101,13 +192,13 @@ fn main() {
     }
 
     let git_checkouts = (cargo_home_path.clone()).join("git/checkouts/");
-    let git_checkouts_size_str = (git_checkouts.clone())
+    let git_checkouts_str = (git_checkouts.clone())
         .into_os_string()
         .into_string()
         .unwrap();
     let mut git_checkouts_size = 0;
     if git_checkouts.is_dir() {
-        git_checkouts_size = cumulative_dir_size(&git_checkouts_size_str);
+        git_checkouts_size = cumulative_dir_size(&git_checkouts_str);
     }
 
     if cargo_show_cfg.is_present("print-dirs") {
@@ -115,10 +206,42 @@ fn main() {
         println!("bin dir: {}", bin_dir_str);
         println!("registry dir: {}", registry_dir_str);
         println!("git db dir: {}", git_db_str);
-        println!("checkouts dir: {}", git_checkouts_size_str);
+        println!("checkouts dir: {}", git_checkouts_str);
     }
 
+    //    let cargo_home_cache = CacheDir { path: &cargo_home_path, string:  &cargo_home_str };
+    let bin_dir_cache = CacheDir {
+        path: &bin_dir,
+        string: &bin_dir_str,
+    };
+    let registry_dir_cache = CacheDir {
+        path: &registry_dir,
+        string: &registry_dir_str,
+    };
+    let git_db_cache = CacheDir {
+        path: &git_db,
+        string: &git_db_str,
+    };
+    let checkouts_cache = CacheDir {
+        path: &git_checkouts,
+        string: &git_checkouts_str,
+    };
 
+    let cargo_cache = CacheDirCollector {
+        GitCheckouts: &checkouts_cache,
+        GitDB: &git_db_cache,
+        Registry: &registry_dir_cache,
+        BinDir: &bin_dir_cache,
+    };
+
+    /*
+
+
+GitCheckouts: &'a CacheDir<'a>,
+GitDB: &'a CacheDir<'a>,
+Registry: &'a CacheDir<'a>,
+BinDir: &'a CacheDir<'a>,
+*/
 
     println!("\nCargo cache:\n");
     println!(
@@ -144,5 +267,12 @@ fn main() {
         "Size of git repo checkouts {} ",
         git_checkouts_size.file_size(options::DECIMAL).unwrap()
     );
+
+
+
+
+    if cargo_show_cfg.is_present("remove-dirs") {
+        rm_dir(&cargo_cache);
+    }
 
 }
