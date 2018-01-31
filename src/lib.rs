587d72866a1e10072fb311ed2715dbb266d727a9
Matthias Krüger <matthias.krueger@famsik.de>
2018-01-31 01:11:59 +0100

    enable additional rustc warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,17 @@
+// enable additional rustc warnings
+#![warn(trivial_casts, trivial_numeric_casts, unsafe_code)]
 // enable additional clippy warnings
 #![cfg_attr(feature = "cargo-clippy", warn(int_plus_one))]
-#![cfg_attr(feature = "cargo-clippy", warn(shadow_reuse))]
-#![cfg_attr(feature = "cargo-clippy", warn(shadow_same))]
-#![cfg_attr(feature = "cargo-clippy", warn(shadow_unrelated))]
+#![cfg_attr(feature = "cargo-clippy", warn(shadow_reuse, shadow_same, shadow_unrelated))]
 #![cfg_attr(feature = "cargo-clippy", warn(mut_mut))]
 #![cfg_attr(feature = "cargo-clippy", warn(nonminimal_bool))]
 #![cfg_attr(feature = "cargo-clippy", warn(pub_enum_variant_names))]
 #![cfg_attr(feature = "cargo-clippy", warn(range_plus_one))]
-#![cfg_attr(feature = "cargo-clippy", warn(string_add))]
-#![cfg_attr(feature = "cargo-clippy", warn(string_add_assign))]
+#![cfg_attr(feature = "cargo-clippy", warn(string_add, string_add_assign))]
 #![cfg_attr(feature = "cargo-clippy", warn(stutter))]
 //#![cfg_attr(feature = "cargo-clippy", warn(result_unwrap_used))]
 
 extern crate cargo;
-extern crate git2;
 extern crate humansize;
 extern crate rayon;
 extern crate walkdir;
