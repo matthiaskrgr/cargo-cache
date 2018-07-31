e3818978757cbafd6c31026e9e6a38328dc0bc98
Matthias Krüger <matthias.krueger@famsik.de>
2018-07-31 14:22:15 +0200

    tests: add a first test.
    add a dummy subcrate with some set dependencies that are known to have no dependencies themselves.
    we then export CARGO_HOME into a new directory and cargo check the dummy crate.
    
    later we can check the sizes and run tests in this dummy CARGO_HOME directory.

---
+++ tests/size_test.rs
@@ -0,0 +1,17 @@
+use std::path::*;
+use std::process::Command;
+
+// note: to make debug prints work:
+// cargo test -- --nocapture
+#[cfg(test)]
+mod sizetests {
+        use super::*;
+
+    #[test]
+    fn build_and_check_size_test() {
+        let crate_path = PathBuf::from("tests/size_test/");
+        let status = Command::new("cargo").arg("check").current_dir(&crate_path).env("CARGO_HOME", "fake_cargo_home").output();
+        assert!(status.is_ok());
+
+    }
+}
