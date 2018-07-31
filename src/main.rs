d20b857b9e0d82ac221658d8c50c5da46ba01df4
Matthias Krüger <matthias.krueger@famsik.de>
2018-07-31 18:55:17 +0200

    clean up clap parsing a bit

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,48 @@ fn main() {
     // parse args
     // dummy subcommand:
     // https://github.com/kbknapp/clap-rs/issues/937
+
+    let list_dirs = Arg::with_name("list-dirs")
+        .short("l")
+        .long("list-dirs")
+        .help("List found directory paths.");
+
+    let remove_dir = Arg::with_name("remove-dir").short("r").long("remove-dir")
+        .help("Remove directories, accepted values: git-db,git-repos,registry-sources,registry-crate-cache,registry,all")
+        .takes_value(true).value_name("dir1,dir2,dir3");
+
+    let gc_repos = Arg::with_name("gc-repos")
+        .short("g")
+        .long("gc")
+        .help("Recompress git repositories (may take some time).");
+    let info = Arg::with_name("info")
+        .short("i")
+        .long("info")
+        .conflicts_with("list-dirs")
+        .help("Give information on directories");
+
+    let keep_duplicate_crates = Arg::with_name("keep-duplicate-crates")
+        .short("k")
+        .long("keep-duplicate-crates")
+        .help("Remove all but N versions of duplicate crates in the source cache")
+        .takes_value(true)
+        .value_name("N");
+
+    let dry_run = Arg::with_name("dry-run")
+        .short("d")
+        .long("dry-run")
+        .help("Don't remove anything, just pretend");
+
+    let autoclean = Arg::with_name("autoclean")
+        .short("a")
+        .long("autoclean")
+        .help("Removes registry src checkouts and git repo checkouts");
+
+    let autoclean_expensive = Arg::with_name("autoclean-expensive")
+        .short("e")
+        .long("autoclean-expensive")
+        .help("Removes registry src checkouts, git repo checkouts and gcs repos");
+
     let config = App::new("cargo-cache")
         .version(crate_version!())
         .bin_name("cargo")
@@ -56,84 +98,27 @@ fn main() {
         .author("matthiaskrgr")
         .subcommand(
             SubCommand::with_name("cache")
-            .version(crate_version!())
-            .bin_name("cargo-cache")
-            .about("Manage cargo cache")
-            .author("matthiaskrgr")
-                .arg(
-                    Arg::with_name("list-dirs")
-                        .short("l")
-                        .long("list-dirs")
-                        .help("List found directory paths."),
-                )
-                .arg(Arg::with_name("remove-dir").short("r").long("remove-dir")
-                    .help("Remove directories, accepted values: git-db,git-repos,registry-sources,registry-crate-cache,registry,all")
-                    .takes_value(true).value_name("dir1,dir2,dir3"),
-                )
-                .arg(Arg::with_name("gc-repos").short("g").long("gc").help(
-                    "Recompress git repositories (may take some time).",
-                ))
-                .arg(
-                    Arg::with_name("info")
-                        .short("i")
-                        .long("info")
-                        .conflicts_with("list-dirs")
-                        .help("Give information on directories"),
-                )
-                .arg(Arg::with_name("keep-duplicate-crates").short("k").long("keep-duplicate-crates")
-                .help("Remove all but N versions of duplicate crates in the source cache")
-                .takes_value(true).value_name("N"),
-                )
-                .arg(
-                    Arg::with_name("dry-run")
-                    .short("d").long("dry-run").help("Don't remove anything, just pretend"),
-                )
-                .arg(
-                    Arg::with_name("autoclean")
-                    .short("a").long("autoclean").help("Removes registry src checkouts and git repo checkouts"),
-                )
-                .arg(
-                    Arg::with_name("autoclean-expensive")
-                    .short("e").long("autoclean-expensive").help("Removes registry src checkouts, git repo checkouts and gcs repos"),
-                ),
-        ) // subcmd
-        .arg(
-            Arg::with_name("list-dirs")
-                .short("l")
-                .long("list-dirs")
-                .help("List found directory paths."),
-        )
-        .arg(Arg::with_name("remove-dir").short("r").long("remove-dir")
-            .help("Remove directories, accepted values: git-db,git-repos,registry-sources,registry-crate-cache,registry,all")
-            .takes_value(true).value_name("dir1,dir2,dir3")
-            ,)
-        .arg(Arg::with_name("gc-repos").short("g").long("gc").help(
-            "Recompress git repositories (may take some time).",
-         ))
-        .arg(
-            Arg::with_name("info")
-                .short("i")
-                .long("info")
-                .conflicts_with("list-dirs")
-                .help("Give information on directories"),
-        )
-        .arg(Arg::with_name("keep-duplicate-crates").short("k").long("keep-duplicate-crates")
-            .help("Remove all but N versions of duplicate crates in the source cache")
-            .takes_value(true).value_name("N"),
-        )
-
-        .arg(
-            Arg::with_name("dry-run")
-            .short("d").long("dry-run").help("Don't remove anything, just pretend"),
-        )
-        .arg(
-            Arg::with_name("autoclean")
-            .short("a").long("autoclean").help("Removes registry src checkouts and git repo checkouts"),
-        )
-        .arg(
-            Arg::with_name("autoclean-expensive")
-            .short("e").long("autoclean-expensive").help("Removes registry src checkouts, git repo checkouts and gcs repos"),
-        )
+                .version(crate_version!())
+                .bin_name("cargo-cache")
+                .about("Manage cargo cache")
+                .author("matthiaskrgr")
+                .arg(&list_dirs)
+                .arg(&remove_dir)
+                .arg(&gc_repos)
+                .arg(&info)
+                .arg(&keep_duplicate_crates)
+                .arg(&dry_run)
+                .arg(&autoclean)
+                .arg(&autoclean_expensive),
+        ) // subcommand
+        .arg(&list_dirs)
+        .arg(&remove_dir)
+        .arg(&gc_repos)
+        .arg(&info)
+        .arg(&keep_duplicate_crates)
+        .arg(&dry_run)
+        .arg(&autoclean)
+        .arg(&autoclean_expensive)
         .get_matches();
 
     // we need this in case we call "cargo-cache" directly
