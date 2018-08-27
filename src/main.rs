b22341dcaf5e17d3ee201f9fb2d6567af2b795ad
Matthias Krüger <matthias.krueger@famsik.de>
2018-08-27 18:49:08 +0200

    final clenaup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-fn pad_strings(indent_lvl: i8, beginning: &str, end: &str) -> String {
+fn pad_strings(indent_lvl: i8, beginning: &str) -> String {
     const MAX_WIDTH: i8 = 37;
     let len_padding: i8 = (MAX_WIDTH + indent_lvl * 2) - (beginning.len() as i8);
-    //assert!(len_padding > 0); // this fires
+    println!("padding: {}", len_padding);
     let mut formatted_line = beginning.to_string();
+    // here we cast a negative value to usize
     formatted_line.push_str(&String::from(" ").repeat(len_padding as usize));
     formatted_line
 }
 
 fn main() {
-    let output_is =
-        pad_strings(2, "Size of 1938493989 crate source checkouts: ", "bla").to_string();
+        pad_strings(2, "Size of 1938493989 crate source checkouts: ").to_string();
 }
