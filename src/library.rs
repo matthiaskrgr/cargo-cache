cf9e653dc39ceb191fedb2e487f9a217c769b442
Matthias Krüger <matthias.krueger@famsik.de>
2018-08-27 05:29:22 +0200

    fix formatting bugs, make use of pretty_assert crate and add more formatting tests.

--- src/library.rs
+++ src/library.rs
@@ -69,6 +69,33 @@ impl DirSizes {
         // and only print it in the end, this should save a few syscalls and be faster than
         // printing every line one by one
 
+        fn pad_strings(indent_lvl: i64, beginning: &str, end: &str) -> String {
+            // max line width
+            const MAX_WIDTH: i64 = 40;
+
+            let len_padding = (MAX_WIDTH + indent_lvl * 2) - (beginning.len() as i64);
+            assert!(
+                len_padding > 0,
+                format!(
+                    "len_padding is negative: '{} - {} = {}' ",
+                    MAX_WIDTH + (indent_lvl * 2),
+                    beginning.len(),
+                    len_padding
+                )
+            );
+
+            let mut formatted_line = beginning.to_string();
+            #[cfg_attr(
+                feature = "cargo-clippy",
+                allow(cast_sign_loss, cast_possible_truncation)
+            )]
+            // I tried mittigating via previous assert()
+            formatted_line.push_str(&String::from(" ").repeat(len_padding as usize));
+            formatted_line.push_str(end);
+            formatted_line.push_str("\n");
+            formatted_line
+        }
+
         // @TODO use format_args!() ?
         let mut s = String::with_capacity(470);
 
@@ -77,68 +104,79 @@ impl DirSizes {
             &cache_root_dir.display()
         ));
 
-        s.push_str(&format!(
-            "Total size: {: >35}\n",
-            self.total_size.file_size(file_size_opts::DECIMAL).unwrap()
+        s.push_str(&pad_strings(
+            0,
+            "Total size: ",
+            &self.total_size.file_size(file_size_opts::DECIMAL).unwrap(),
         ));
 
-        // the nested format!()s are a hack to get nice alignment of the numbers
-        // any ideas on how to not uses nested format here is appreciate...
-        s.push_str(&format!(
-            "{: <41} {}\n",
-            &format!("Size of {} installed binaries:", self.numb_bins,),
-            self.total_bin_size
+        s.push_str(&pad_strings(
+            1,
+            &format!("Size of {} installed binaries: ", self.numb_bins),
+            &self
+                .total_bin_size
                 .file_size(file_size_opts::DECIMAL)
-                .unwrap()
+                .unwrap(),
         ));
 
-        s.push_str(&format!(
-            "Size of registry: {: >33}\n",
-            self.total_reg_size
+        s.push_str(&pad_strings(
+            1,
+            "Size of registry: ",
+            &self
+                .total_reg_size
                 .file_size(file_size_opts::DECIMAL)
-                .unwrap()
+                .unwrap(),
         ));
 
-        s.push_str(&format!(
-            "{: <44}{}\n",
-            &format!("Size of {} crate archives:", self.numb_reg_cache_entries),
-            self.total_reg_cache_size
+        s.push_str(&pad_strings(
+            2,
+            &format!("Size of {} crate archives: ", self.numb_reg_cache_entries),
+            &self
+                .total_reg_cache_size
                 .file_size(file_size_opts::DECIMAL)
-                .unwrap()
+                .unwrap(),
         ));
 
-        s.push_str(&format!(
-            "{: <43} {}\n",
+        s.push_str(&pad_strings(
+            2,
             &format!(
-                "Size of {} crate source checkouts:",
+                "Size of {} crate source checkouts: ",
                 self.numb_reg_src_checkouts
             ),
-            self.total_reg_src_size
+            &self
+                .total_reg_src_size
                 .file_size(file_size_opts::DECIMAL)
-                .unwrap()
+                .unwrap(),
         ));
 
-        s.push_str(&format!(
-            "Size of git db: {: >35}\n",
-            self.total_git_db_size
+        s.push_str(&pad_strings(
+            1,
+            "Size of git db: ",
+            &self
+                .total_git_db_size
                 .file_size(file_size_opts::DECIMAL)
-                .unwrap()
+                .unwrap(),
         ));
 
-        s.push_str(&format!(
-            "{: <43} {}\n",
-            &format!("Size of {} bare git repos:", self.numb_git_repos_bare_repos),
-            self.total_git_repos_bare_size
+        s.push_str(&pad_strings(
+            2,
+            &format!(
+                "Size of {} bare git repos: ",
+                self.numb_git_repos_bare_repos
+            ),
+            &self
+                .total_git_repos_bare_size
                 .file_size(file_size_opts::DECIMAL)
-                .unwrap()
+                .unwrap(),
         ));
 
-        s.push_str(&format!(
-            "{: <43} {}", /* final println already introduces \n */
-            &format!("Size of {} git repo checkouts:", self.numb_git_checkouts),
-            self.total_git_chk_size
+        s.push_str(&pad_strings(
+            2,
+            &format!("Size of {} git repo checkouts: ", self.numb_git_checkouts),
+            &self
+                .total_git_chk_size
                 .file_size(file_size_opts::DECIMAL)
-                .unwrap()
+                .unwrap(),
         ));
 
         s
@@ -822,6 +860,7 @@ pub(crate) fn get_top_crates(limit: u32, ccd: &CargoCachePaths) -> String {
 #[cfg(test)]
 mod libtests {
     use super::*;
+    use pretty_assertions::assert_eq;
     use test::Bencher;
 
     impl DirSizes {
@@ -924,12 +963,12 @@ mod libtests {
 
 Total size:                             1.94 GB
 Size of 31 installed binaries:            121.21 KB
-Size of registry:                           1.94 GB
+Size of registry:                         1.94 GB
 Size of 23445 crate archives:               89 B
 Size of 123909849 crate source checkouts:   1.94 GB
 Size of git db:                           156.20 KB
 Size of 37 bare git repos:                  121.21 KB
-Size of 8 git repo checkouts:               34.98 KB";
+Size of 8 git repo checkouts:               34.98 KB\n";
 
         assert_eq!(output_is, output_should);
     }
@@ -978,4 +1017,117 @@ Size of 8 git repo checkouts:               34.98 KB";
             dir_sizes.print_pretty(&cache_root);
         })
     }
+
+    #[allow(non_snake_case)]
+    #[test]
+    fn test_DirSizes_gigs() {
+        // DirInfors to construct DirSizes from
+        let bindir = DirInfo {
+            dir_size: 6_4015_8118,
+            file_number: 69,
+        };
+        let git_repos_bare = DirInfo {
+            dir_size: 30_961_3689,
+            file_number: 123,
+        };
+        let git_checkouts = DirInfo {
+            dir_size: 39_2270_2821,
+            file_number: 36,
+        };
+        let reg_cache = DirInfo {
+            dir_size: 5_5085_5781,
+            file_number: 3654,
+        };
+        let reg_src = DirInfo {
+            dir_size: 90_559_6846,
+            file_number: 1615,
+        };
+        let reg_index = DirInfo {
+            dir_size: 23,
+            file_number: 0,
+        };
+
+        // create a DirSizes object
+        let dirSizes = DirSizes::new_manually(
+            &bindir,
+            &git_repos_bare,
+            &git_checkouts,
+            &reg_cache,
+            &reg_src,
+            &reg_index,
+        );
+
+        let cache_root = PathBuf::from("/home/user/.cargo");
+        let output_is = dirSizes.print_pretty(&cache_root);
+
+        let output_should = "Cargo cache '/home/user/.cargo/':
+
+Total size:                             6.33 GB
+Size of 69 installed binaries:            640.16 MB
+Size of registry:                         1.46 GB
+Size of 3654 crate archives:                550.86 MB
+Size of 1615 crate source checkouts:        905.60 MB
+Size of git db:                           4.23 GB
+Size of 123 bare git repos:                 309.61 MB
+Size of 36 git repo checkouts:              3.92 GB\n";
+
+        assert_eq!(output_is, output_should);
+    }
+
+    #[allow(non_snake_case)]
+    #[test]
+    fn test_DirSizes_almost_empty() {
+        // DirInfors to construct DirSizes from
+        let bindir = DirInfo {
+            dir_size: 0,
+            file_number: 0,
+        };
+        let git_repos_bare = DirInfo {
+            dir_size: 0,
+            file_number: 0,
+        };
+        let git_checkouts = DirInfo {
+            dir_size: 0,
+            file_number: 0,
+        };
+        let reg_cache = DirInfo {
+            dir_size: 130_4234_1234,
+            file_number: 4,
+        };
+        let reg_src = DirInfo {
+            dir_size: 2_6846_1234,
+            file_number: 4,
+        };
+        let reg_index = DirInfo {
+            dir_size: 125_5000_000,
+            file_number: 1,
+        };
+
+        // create a DirSizes object
+        let dirSizes = DirSizes::new_manually(
+            &bindir,
+            &git_repos_bare,
+            &git_checkouts,
+            &reg_cache,
+            &reg_src,
+            &reg_index,
+        );
+
+        let cache_root = PathBuf::from("/home/user/.cargo");
+        let output_is = dirSizes.print_pretty(&cache_root);
+
+        let output_should = "Cargo cache '/home/user/.cargo/':
+
+Total size:                             14.57 GB
+Size of 0 installed binaries:             0 B
+Size of registry:                         14.57 GB
+Size of 4 crate archives:                   13.04 GB
+Size of 4 crate source checkouts:           268.46 MB
+Size of git db:                           0 B
+Size of 0 bare git repos:                   0 B
+Size of 0 git repo checkouts:               0 B\n";
+
+        assert_eq!(output_is, output_should);
+    }
+
 }
