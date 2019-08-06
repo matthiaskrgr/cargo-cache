b6579a24c3e87e177f73bbfe0da9d0e2841c62e4
Matthias Krüger <matthias.krueger@famsik.de>
2019-08-06 23:16:23 +0200

    document build.rs

--- src/build.rs
+++ src/build.rs
@@ -7,12 +7,17 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+/// provide the date of the latest commit and its git hash to the application
+/// so we can have it in --version output
 fn main() {
+    // rebuild the crate if src/build.rs changed
     println!("cargo:rerun-if-changed=src/build.rs");
+    // obtain the git hash
     println!(
         "cargo:rustc-env=GIT_HASH={}",
         rustc_tools_util::get_commit_hash().unwrap_or_default()
     );
+    // obtain the commit date
     println!(
         "cargo:rustc-env=COMMIT_DATE={}",
         rustc_tools_util::get_commit_date().unwrap_or_default()
