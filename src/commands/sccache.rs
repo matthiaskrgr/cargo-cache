1e4f40cca99fbfa623dbc0d43b5c3720b69cbd61
Matthias Krüger <matthias.krueger@famsik.de>
2020-05-20 21:58:43 +0200

    sccache: add function to get sccache stats

--- src/commands/sccache.rs
+++ src/commands/sccache.rs
@@ -9,6 +9,77 @@
 
 // find ~/.cache/sccache -type f -printf "\n%AD %AT %p"  | cut -d' ' -f1 | sort -n | uniq -c
 
+use std::fs;
+use std::path::PathBuf;
+
+use chrono::prelude::*;
+use walkdir::WalkDir;
+
+#[derive(Debug, Clone)]
+struct File {
+    path: PathBuf,
+    access_date: NaiveDate,
+}
+
 pub(crate) fn sccache_stats() {
-    unimplemented!();
+    let sccache_path: PathBuf = unimplemented!();
+
+    // we need to get all the files in the cache
+    // get path, creation time and access time
+    let files = WalkDir::new(sccache_path.display().to_string())
+        .into_iter()
+        .filter_map(|f| {
+            if let Ok(direntry) = f {
+                let path = direntry.path().to_path_buf();
+                if path.is_file() {
+                    if let Ok(metadata) = fs::metadata(&path) {
+                        if let Ok(access_time) = metadata.accessed() {
+                            // let creation_time =
+                            //   chrono::DateTime::<Local>::from(create_time).naive_local();
+                            let access_time =
+                                chrono::DateTime::<Local>::from(access_time).naive_local();
+                            let access_date = access_time.date();
+                            return Some(File {
+                                path,
+                                // creation_time,
+                                access_date,
+                            });
+                        }
+                    }
+                }
+            };
+
+            None
+        });
+
+    let files_sorted = {
+        let mut v: Vec<File> = files.collect();
+        v.sort_by_key(|file| file.access_date);
+        v
+    };
+
+    let mut unique = files_sorted.clone();
+    unique.dedup_by_key(|f| f.access_date);
+
+    let unique: Vec<NaiveDate> = unique.into_iter().map(|f| f.access_date).collect();
+
+    let date_occurrences: Vec<(usize, &NaiveDate)> = unique
+        .iter()
+        .map(|unique_date| {
+            let count = files_sorted
+                .iter()
+                .filter(|f| f.access_date == *unique_date)
+                .count();
+
+            (count, unique_date)
+        })
+        .collect();
+
+    date_occurrences
+        .iter()
+        .map(|x| x)
+        // .filter(|x| x.access_time != x.creation_time)
+        .for_each(|x| {
+            println!("{:?}", x);
+        });
 }
