5a1941a94cbf01a4d82d193e24ad643eef3c1a4d
Matthias Krüger <matthias.krueger@famsik.de>
2020-06-04 21:02:16 +0200

    map from source checkouts to sources in the cargo_home

--- src/clear_unref.rs
+++ src/clear_unref.rs
@@ -103,10 +103,49 @@ pub(crate) fn clear_unref(cargo_cache_paths: &CargoCachePaths) -> Result<(), Err
                     toml_path.display()
                 );
             }
+        })
+        // we need to map the git repo checkouts to bare git repos
+        // and the source-checkouts to pkg cache archives!
+        .map(|sourcekind| match sourcekind {
+            SourceKind::Crate(registry_src_path) => {
+                // ~/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.9.0
+                // =>
+                // ~/.cargo/registry/cache/github.com-1ecc6299db9ec823/semver-0.9.0.crate
+                let path = registry_src_path.iter().collect::<Vec<&OsStr>>();
+                let package_name = path[path.len() - 1];
+                let registry = path[path.len() - 2];
+                let mut registry_cache_path = cargo_cache_paths.registry_pkg_cache.clone();
+                // we need to push the registry index as well
+                registry_cache_path.push(registry);
+                // this can probably be
+                // can't use .set_extension() here because "cratename-0.1.3" will detect the ".3" as extension
+                // and change it
+                registry_cache_path.push(format!(
+                    "{}{}",
+                    package_name.to_os_string().into_string().unwrap(),
+                    ".crate"
+                ));
+                SourceKind::Crate(registry_cache_path)
+            }
+            SourceKind::Git(gitpath) => {
+                // ~/.cargo/git/checkouts/cargo-e7ff1db891893a9e/258c896
+                // =>
+                // ~/.cargo/git/db/cargo-e7ff1db891893a9e
+                let mut repo_name = gitpath;
+                let _ = repo_name.pop(); // remove /258c896
+                let repo_name = repo_name.iter().last().unwrap(); // cargo-e7ff1db891893a9e
+
+                let mut db_name = cargo_cache_paths.git_repos_bare.clone();
+                db_name.push(repo_name);
+                // ~/.cargo/git/db/cargo-e7ff1db891893a9e
+                SourceKind::Git(db_name)
+            }
         });
 
     // now we have a list of all cargo-home-entries a crate needs to build
-    // we can walk the cargo-cache and remove everything that is not referenced
+    // we can walk the cargo-cache and remove everything that is not referenced;
+    // remove: git checkouts, registry sources
+    // keep, if referenced: registry pkg cache, bare git repos
 
     // debug
     required_packages.for_each(|toml| println!("{:?}", toml));
