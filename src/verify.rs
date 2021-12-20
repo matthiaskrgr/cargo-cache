77cec65120a73c32e822ccce0d2cb8511b93d019
Matthias Krüger <matthias.krueger@famsik.de>
2021-12-20 01:07:10 +0100

    verify: impl .crate archive reading

--- src/verify.rs
+++ src/verify.rs
@@ -1,12 +1,38 @@
 use crate::cache::caches::Cache;
 use crate::cache::caches::RegistrySubCache;
 use crate::cache::caches::RegistrySuperCache;
-
 use crate::cache::*;
 use crate::library::*;
+
+use flate2::read::GzDecoder;
+use tar::Archive;
+
 use std::ffi::{OsStr, OsString};
+use std::fs::File;
 use std::path::PathBuf;
 
+#[derive(Debug, Clone)]
+struct FileWithSize {
+    path: PathBuf,
+    size: u64,
+}
+
+impl FileWithSize {
+    fn from_disk(path: &PathBuf) -> Self {
+        FileWithSize {
+            path: path.clone(),
+            size: std::fs::metadata(path).unwrap().len(),
+        }
+    }
+
+    // TODO: understand this R: Read stuff
+    fn from_archive<'a, R: std::io::Read>(entry: &tar::Entry<'a, R>) -> Self {
+        FileWithSize {
+            path: entry.path().unwrap().into_owned(),
+            size: entry.size(),
+        }
+    }
+}
 pub(crate) fn verify_crates(
     registry_pkg_caches: &mut registry_pkg_cache::RegistryPkgCaches,
     registry_sources_caches: &mut registry_sources::RegistrySourceCaches,
@@ -48,7 +74,38 @@ pub(crate) fn verify_crates(
     // this would fail if we for example have a crate source dir but no corresponding archive
     assert_eq!(crate_gzips_and_sources.len(), reg_sources.len());
 
-    crate_gzips_and_sources.iter().map(|(source, krate)| {});
+    crate_gzips_and_sources
+        .iter()
+        .map(|(source, krate)| {
+            let files_of_archive = {
+                let tar_gz = File::open(krate).unwrap();
+                // extract the tar
+                let tar = GzDecoder::new(tar_gz);
+                let mut archive = Archive::new(tar);
+
+                let archive_files = archive.entries().unwrap();
+
+                let x: Vec<_> = archive_files
+                    .into_iter()
+                    .map(|entry| {
+                        let e = entry.unwrap();
+                        e.path().unwrap().into_owned()
+                    })
+                    .collect();
+                x
+            };
+
+            let files_of_source: Vec<_> = std::fs::read_dir(source)
+                .unwrap()
+                .map(|direntry| {
+                    let x = direntry.unwrap();
+                    x.path()
+                })
+                .collect();
+
+            println!("{:?}|||||{:?}", files_of_archive, files_of_source);
+        })
+        .collect::<Vec<_>>();
 
     if false {
         return Err(());
