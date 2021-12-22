86c6f169dd3291536d1f6060b0fad73d57d7ac29
Matthias Krüger <matthias.krueger@famsik.de>
2021-12-22 19:55:28 +0100

    verify: refactor
    move some code to a fn map_src_path_to_cache_path()

--- src/verify.rs
+++ src/verify.rs
@@ -119,47 +119,44 @@ impl Diff {
         s
     }
 }
+
+/// take a path to an extracted .crate source and map it to the corresponding .carte archive path
+fn map_src_path_to_cache_path(src_path: &PathBuf) -> PathBuf {
+    // for each directory, find the path to the corresponding .crate archive
+    // .cargo/registry/src/github.com-1ecc6299db9ec823/bytes-0.4.12
+    // corresponds to
+    // .cargo/registry/cache/github.com-1ecc6299db9ec823/bytes-0.4.12.crate
+
+    // reverse, and "pop" the front components
+    let mut dir = src_path.iter().collect::<Vec<&OsStr>>();
+
+    let comp1 = dir.pop().unwrap(); // /bytes-0.4.12
+    let comp2 = dir.pop().unwrap(); // github.com-1ecc6299db9ec823
+    let _src = dir.pop().unwrap(); // throw this away and add "cache" instead
+
+    // reconstruct the fixed path in reverse order
+
+    dir.push(OsStr::new("cache"));
+    dir.push(comp2); // github.com...
+                     // we need to add the .crate extension (path to the gzip archive)
+    let mut comp1_with_crate_ext = comp1.to_os_string();
+    comp1_with_crate_ext.push(".crate");
+
+    dir.push(&comp1_with_crate_ext); // bytes-0.4.12.crate
+    dir.into_iter().collect::<PathBuf>()
+}
 pub(crate) fn verify_crates(
     registry_sources_caches: &mut registry_sources::RegistrySourceCaches,
 ) -> Result<(), Vec<Diff>> {
     // iterate over all the extracted sources that we have
 
-    let reg_sources = registry_sources_caches.items();
-    let bad_sources: Vec<_> = reg_sources
+    let bad_sources: Vec<_> = registry_sources_caches
+        .items()
         .par_iter()
         // get the paths to the source and the .crate for all extracted crates
-        .map(|source| {
-            // for each directory, find the path to the corresponding .crate archive
-            // .cargo/registry/src/github.com-1ecc6299db9ec823/bytes-0.4.12
-            // corresponds to
-            // .cargo/registry/cache/github.com-1ecc6299db9ec823/bytes-0.4.12.crate
-
-            // reverse, and "pop" the front components
-            let mut dir = source.iter().collect::<Vec<&OsStr>>();
-
-            let comp1 = dir.pop().unwrap(); // /bytes-0.4.12
-            let comp2 = dir.pop().unwrap(); // github.com-1ecc6299db9ec823
-            let _src = dir.pop().unwrap(); // throw this away and add "cache" instead
-
-            // reconstruct the fixed path in reverse order
-
-            dir.push(OsStr::new("cache"));
-            dir.push(comp2); // github.com...
-                             // we need to add the .crate extension (path to the gzip archive)
-            let mut comp1_with_crate_ext = comp1.to_os_string();
-            comp1_with_crate_ext.push(".crate");
-
-            dir.push(&comp1_with_crate_ext); // bytes-0.4.12.crate
-            let krate: PathBuf = dir.into_iter().collect::<PathBuf>();
-            (source, krate)
-        })
-        // we need both the .crate and the directory for verification
+        .map(|source| (source, map_src_path_to_cache_path(source)))
+        // we need both the .crate and the directory to exist for verification
         .filter(|(source, krate)| source.exists() && krate.exists())
-        //  .collect();
-        // this would fail if we for example have a crate source dir but no corresponding archive
-        //  assert_eq!(crate_gzips_and_sources.len(), reg_sources.len());
-        //let _x = crate_gzips_and_sources
-        //  .iter()
         .map(|(source, krate)| {
             let krate_name = source.iter().last().unwrap();
             //println!("Verifying: {}", &krate_name.to_str().unwrap());
