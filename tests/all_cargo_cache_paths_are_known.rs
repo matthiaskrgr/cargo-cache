c45eaf83bcb33b12e2db49679aacb03cbc7a1efb
Matthias Krüger <matthias.krueger@famsik.de>
2023-03-25 11:27:04 +0100

    ignore some tests on more version

--- tests/all_cargo_cache_paths_are_known.rs
+++ tests/all_cargo_cache_paths_are_known.rs
@@ -24,7 +24,7 @@ fn CARGO_HOME_subdirs_are_known() {
     let version_output = String::from_utf8_lossy(&cargo_v.stdout).to_string();
 
     //https://github.com/rust-lang/cargo/pull/10553
-    let disallowed_versions = ["1.57", "1.60", "1.61"];
+    let disallowed_versions = ["1.57", "1.60", "1.61", "1.68", "1.69"];
     if disallowed_versions
         .iter()
         .any(|version| version_output.contains(version))
